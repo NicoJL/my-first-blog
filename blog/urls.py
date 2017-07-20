d1092704a1b843a394bc67a2e3efc00538ca9381
nicojl <njl27@hotmail.com>
2017-07-20 15:12:42 -0700

    edicion y alta

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.post_list),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail,name='postdetail'),
+	url(r'^post/new/$', views.post_new,name='postnew'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit,name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,52 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.all().order_by('published_date')
 	return render(request,'blog/post_list.html', {'posts':posts})
+
+def post_detail(request,pk):
+	#posts = Post.objects.get(pk=pk)	
+	posts = get_object_or_404(Post,pk=pk)
+	return render(request,'blog/post_detail.html', {'posts':posts})
+
+def post_new(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.autor = request.user
+			post.published_date = timezone.now()
+			post.save()
+			pk=post.pk
+			return redirect(reverse('blog:postdetail',args=[pk]))
+	else:
+		form = PostForm()
+	#form = PostForm()
+	#posts = get_object_or_404(Post,pk=pk)
+	return render(request,'blog/post_edit.html', {'form':form})
+
+def post_edit(request,pk):
+	post = get_object_or_404(Post,pk=pk)
+	if request.method == 'POST':
+		form = PostForm(request.POST,instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.autor = request.user
+			#post.published_date = timezone.now()
+			post.save()
+			pk=post.pk
+			return redirect(reverse('blog:postdetail',args=[pk]))
+	else:
+		form = PostForm(instance=post)
+		#form = PostForm()
+		#posts = get_object_or_404(Post,pk=pk)
+	return render(request,'blog/post_edit.html', {'form':form})
+
