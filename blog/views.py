943e2270a753cbec529c2b7cf43c46affb8e0c3c
nicojl <njl27@hotmail.com>
2017-07-20 09:54:34 -0700

    post

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+	posts = Post.objects.all().order_by('published_date')
+	return render(request,'blog/post_list.html', {'posts':posts})
